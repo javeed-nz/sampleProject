929acc2ece78059b0c41b2730ac4d43eb30261ab
syed javeed <syedjaveed@nextgen-is.com>
2024-03-18 14:52:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+
+  return (
+    <>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      width: '100%',
+      flexDirection: 'column'
+    }}>
+      <h1>Sample Page</h1>
+      <h2>this is powered by nginx</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
